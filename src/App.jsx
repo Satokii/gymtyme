d48d868769ec631bf5c9c0da8f45998eb5308fb8
Satoki <satokiito@gmail.com>
2023-11-27 22:05:32 +0000

    experimenting with api to see if it is useable in this application

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import Header from './components/header'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
 
+  const [workout, setWorkout] = useState([])
+
+  // INCLUDE A WORKOUT REFRESH BUTTON SO USER CAN CHANGE LIST OF WORKOUTS
+
+  useEffect(() => {
+    fetch('https://wger.de/api/v2/exercise/')
+    .then(res => res.json())
+    .then(data => setWorkout(data.results))
+  }, [])
+  
+workout.map(y => console.log(y.name))
+
   return (
     <>
-    <div className='container grid'>
-      <Header />
-      <main>I am the main</main>
-      <footer>Here lies the footer</footer>
-    </div>
-      
+      <div className='container grid'>
+        <Header />
+        <main>I am the main</main>
+        <footer>Here lies the footer</footer>
+      </div>
     </>
   )
 }
