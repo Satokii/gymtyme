2c3b69d680325893c5957269a1bae1c108f83a9a
Satoki <satokiito@gmail.com>
2023-11-28 03:58:56 +0000

    added sets/reps counters to exercise suggestions for user to increase/decrease

--- src/components/exercises/index.jsx
+++ src/components/exercises/index.jsx
@@ -10,6 +10,22 @@ function Exercises({ exercises }) {
                     {exercises.map((exercise, index) =>
                         <li className='exercise-item-container grid' key={`${exercise.id}-${index}`}>
                             <p className='exercise-name'>{exercise.name}</p>
+                            <div className='sets-container grid'>
+                                <p>Sets</p>
+                                <div className='count-container grid'>
+                                    <button className='minus-btn grid'>-</button>
+                                    <span className='current-val grid'>0</span>
+                                    <button className='add-btn grid'>+</button>
+                                </div>
+                            </div>
+                            <div className='reps-container grid'>
+                                <p>Reps</p>
+                                <div className='count-container grid'>
+                                    <button className='minus-btn grid'>-</button>
+                                    <span className='current-val grid'>0</span>
+                                    <button className='add-btn grid'>+</button>
+                                </div>
+                            </div>
                             <div className='exercise-btn-container'>
                                 <button className='add-to-workout-btn'>Add to Workout</button>
                             </div>
