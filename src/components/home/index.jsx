88774c6dff506167ae557c886f002ce6eaf03b37
Satoki <satokiito@gmail.com>
2023-11-30 19:00:50 +0000

    hidden header and footer on very first page load

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -6,7 +6,6 @@ import '../../styles/home.css'
 
 function Home({ showInitialDisplay, setShowInitialDisplay }) {
 
-    // const [showInitialDisplay, setShowInitialDisplay] = useState(true)
     const [gif, setGif] = useState(true)
 
     const navigate = useNavigate()
@@ -43,26 +42,6 @@ function Home({ showInitialDisplay, setShowInitialDisplay }) {
                 </div>    
             </div>
             }
-             {/* <div className='home-cover-menu'>
-                 <p>Welcome to <span>GymTyme</span></p>
-                 <a className='click-to-start' href="#home">Click to Start</a>
-             </div> */}
-            {/* <div id='home' className='home-container'>
-                <div className='content grid'>
-                    <h3 className='home-what-to-do-header grid'>What would you like to do?</h3>
-                    <ul className='home-options-list grid'>
-                        <li onClick={() => navigate('/workouts')}>Go to the workouts page</li>
-                        <li onClick={() => navigate('/exercises')}>See exercise suggestions</li>
-                        <li>View your previous workouts</li>
-                    </ul>
-                    <div onMouseOver={() => toggleGif()}>
-                        {gif ? 
-                        <img className='home-gif home-gif-main' src={HomeGif} alt="home gif" /> 
-                        : <img className='home-gif home-gif-alt' src={HomeGifAlt} alt="home gif alt" />
-                        }
-                    </div>
-                </div>    
-            </div> */}
         </section>
     )
 }
