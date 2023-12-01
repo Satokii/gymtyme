1c70ff6bd5d0baf32913904ea09dc2219418ccb5
Satoki <satokiito@gmail.com>
2023-12-01 01:30:03 +0000

    fixed clicking home page links now causing linked page to start at top of window

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -12,6 +12,7 @@ function Home({ showInitialDisplay, setShowInitialDisplay }) {
 
     function handlePageDisplay() {
         setShowInitialDisplay(false)
+        
     }
 
     function toggleGif() {
@@ -29,8 +30,14 @@ function Home({ showInitialDisplay, setShowInitialDisplay }) {
                 <div className='content grid'>
                     <h3 className='home-what-to-do-header grid'>What would you like to do?</h3>
                     <ul className='home-options-list btn-ani-target grid'>
-                        <li onClick={() => navigate('/workouts')}><span></span><span></span><span></span><span></span>Go To The Workouts Page </li>
-                        <li onClick={() => navigate('/exercises')}><span></span><span></span><span></span><span></span>See Exercise Suggestions</li>
+                        <li onClick={() => {
+                            navigate('/workouts')
+                            window.scrollTo(0, 0)
+                        }}><span></span><span></span><span></span><span></span>Go To The Workouts Page </li>
+                        <li onClick={() => {
+                            navigate('/exercises')
+                            window.scrollTo(0, 0)
+                        }}><span></span><span></span><span></span><span></span>See Exercise Suggestions</li>
                         <li><span></span><span></span><span></span><span></span>View Your Previous Workouts</li>
                     </ul>
                     <div onMouseOver={() => toggleGif()}>
