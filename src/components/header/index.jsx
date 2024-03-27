a8b57e4ebce12ca3b1a1e150a46b47422cbef598
Satoki <satokiito@gmail.com>
2024-03-27 16:37:52 +0000

    feat: add hamburger menu and associated media queries to header for smaller screens

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,64 +1,69 @@
-import '../../styles/header.css'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import Logo from './components/logo';
+import '../../styles/header.css';
 
 function Header({ setShowPage }) {
-
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const toggleClassName = ({ isActive }) => (isActive ? "active_link" : "inactive-link");
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        window.scrollTo(0, 0);
+    };
 
     return (
         <header className="header grid">
-            <h1 className='header-logo' onClick={() => {
-                window.scrollTo(0, 100)
-                navigate('/')
-            }}>
-                <span>G</span>
-                <span>y</span>
-                <span>m</span>
-                <span>T</span>
-                <span>y</span>
-                <span>m</span>
-                <span>e</span>
-            </h1>
-            <nav className='header-nav grid'>
+            <Logo />
+            <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+            <nav className={`header-nav grid ${isOpen ? 'open' : ''}`}>
                 <div className='header-nav-list grid'>
                     <NavLink
                         className={toggleClassName}
                         to="/"
-                        onClick={() => window.scrollTo(0, 100)}
+                        onClick={closeMenu}
                     >
                         Home
                     </NavLink>
                     <NavLink
                         className={toggleClassName}
                         to="/workouts"
-                        onClick={() => window.scrollTo(0, 0)}
+                        onClick={closeMenu}
                     >
                         Workouts
                     </NavLink>
                     <NavLink
                         className={toggleClassName}
                         to="/exercises"
-                        onClick={() => window.scrollTo(0, 0)}
+                        onClick={closeMenu}
                     >
                         Exercises
                     </NavLink>
                     <NavLink
                         className={toggleClassName}
                         to='/completed-workouts'
-                        onClick={() => window.scrollTo(0, 0)}
+                        onClick={closeMenu}
                     >
                         Completed Workouts
                     </NavLink>
                 </div>
             </nav>
             <div className='header-profile grid' onClick={() => {
-                window.scrollTo(0, 0)
-                navigate('/profile')
-                setShowPage('profile')
+                window.scrollTo(0, 0);
+                navigate('/profile');
+                setShowPage('profile');
             }}>Profile</div>
         </header>
     )
 }
 
-export default Header
+export default Header;
