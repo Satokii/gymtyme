918e4fd77027ae356062006a84b300ae31f38d27
Satoki <satokiito@gmail.com>
2024-04-02 18:11:27 +0100

    style: add base styling to favourite exercise page

--- src/components/profile/components/FavouriteExercises.jsx
+++ src/components/profile/components/FavouriteExercises.jsx
@@ -1,26 +1,25 @@
 import '../../../styles/profile/favourite-exercises.css'
 
 function FavouriteExercises({ favourites }) {
-    console.log(favourites)
 
     return (
         <section className='profile--favourites grid'>
-            <h2>Favourite Exercises</h2>
-            <div>
-                <div>
+            <h2 className='profile--favourites-header'>Favourite Exercises</h2>
+            <div className='profile--favourites-content grid'>
+                <div className='profile--favourites-headers grid'>
                     <h3>Exercise</h3>
                     <h3>Sets</h3>
                     <h3>Reps</h3>
                     <h3>Muscle Group</h3>
+                    <h3>Add to Workout</h3>
                 </div>
-            </div>
-            <div>
                 {favourites.map((exercise) => (
-                    <div key={exercise.id}>
+                    <div className='profile--favourites-exercises grid' key={exercise.id}>
                         <p>{exercise.name}</p>
                         <p>{exercise.reps}</p>
                         <p>{exercise.sets}</p>
                         <p>{exercise.group}</p>
+                        <p>Add</p>
                     </div>
                 ))}
             </div>
