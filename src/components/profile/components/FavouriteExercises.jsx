204be14c244127a5b5637b5716821d2b25d73c60
Satoki <satokiito@gmail.com>
2024-04-02 17:45:37 +0100

    feat: add base content to favourite exercise page

--- src/components/profile/components/FavouriteExercises.jsx
+++ src/components/profile/components/FavouriteExercises.jsx
@@ -6,6 +6,24 @@ function FavouriteExercises({ favourites }) {
     return (
         <section className='profile--favourites grid'>
             <h2>Favourite Exercises</h2>
+            <div>
+                <div>
+                    <h3>Exercise</h3>
+                    <h3>Sets</h3>
+                    <h3>Reps</h3>
+                    <h3>Muscle Group</h3>
+                </div>
+            </div>
+            <div>
+                {favourites.map((exercise) => (
+                    <div key={exercise.id}>
+                        <p>{exercise.name}</p>
+                        <p>{exercise.reps}</p>
+                        <p>{exercise.sets}</p>
+                        <p>{exercise.group}</p>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
