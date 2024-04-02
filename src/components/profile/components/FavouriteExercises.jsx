ae26492976268257ae15adb03c7c7c025c22bbd2
Satoki <satokiito@gmail.com>
2024-04-02 17:24:28 +0100

    build: add favourite exercises component

---
+++ src/components/profile/components/FavouriteExercises.jsx
@@ -0,0 +1,10 @@
+function FavouriteExercises() {
+
+    return (
+        <section id='fourth' className='profile--favourites grid'>
+            <h3>🚧🚧👷🚧🚧 <br /> Favourites tab still under construction <br /> 🚧🚧👷🚧🚧</h3>
+        </section>
+    )
+}
+
+export default FavouriteExercises
